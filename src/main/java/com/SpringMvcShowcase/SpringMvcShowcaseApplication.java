c9693028a920fde96361af2337e289de63bdfc2f
Szymon Czarnecki <sczarnecki091@gmail.com>
2025-05-20 11:15:49 +0200

    chore: inital commit

---
+++ src/main/java/com/SpringMvcShowcase/SpringMvcShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.SpringMvcShowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringMvcShowcase/SpringMvcShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringMvcShowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
